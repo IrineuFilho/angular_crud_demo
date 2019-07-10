f25ffe321f7cb52c14490f4ba376113b1900ccf9
Irineu Filho <irineutenorio@gmail.com>
2019-07-10 01:56:42 -0300

    Create ConfirmationDialog on SharedModule to use the dialog in any place on the system. In this commit, it will be used on Remove Customer's Action

---
+++ src/app/shared/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,28 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrls: ['./confirmation-dialog.component.scss']
+})
+export class ConfirmationDialogComponent implements OnInit {
+
+  constructor(public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any) {
+  }
+
+  ngOnInit() {
+  }
+
+  closeDialog(choice) {
+    this.dialogRef.close({result: choice});
+  }
+
+  title() {
+   return this.data.title;
+  }
+  message() {
+    return this.data.message;
+  }
+}
