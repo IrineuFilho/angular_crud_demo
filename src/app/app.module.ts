685e0e3ae785029644a61f78679fdcfefb30e17a
Irineu Filho <irineutenorio@gmail.com>
2019-07-11 00:45:31 -0300

    Loader Service, Loader Component and Add MatSnackBarDefaultOptions, NgxLoading and LoaderService on App.Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,9 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {SharedModule} from './shared/shared.module';
 import {AppComponent} from './app.component';
-
+import {MAT_SNACK_BAR_DEFAULT_OPTIONS} from '@angular/material';
+import {NgxLoadingModule} from 'ngx-loading';
+import {LoaderService} from './loader.service';
 
 @NgModule({
   declarations: [
@@ -14,8 +16,12 @@ import {AppComponent} from './app.component';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
+    NgxLoadingModule.forRoot({})
   ],
-  providers: [],
+  providers: [
+    LoaderService,
+    {provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {duration: 2500}}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
