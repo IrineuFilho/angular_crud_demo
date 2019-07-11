3aa906e0dc69c79b3b5d06d7b01be859eecdfc5a
Irineu Filho <irineutenorio@gmail.com>
2019-07-11 00:52:51 -0300

    Add new Modules on CustomersModule, form to create/update a customer is finished

--- src/app/customers/customer-dialog/customer-dialog.component.ts
+++ src/app/customers/customer-dialog/customer-dialog.component.ts
@@ -1,5 +1,9 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {MAT_DIALOG_DATA, MatDialogRef, MatSnackBar} from '@angular/material';
+import {FormBuilder, Validators} from '@angular/forms';
+import {EmailValidator} from '../../shared/validators/email-validator.directive';
+import {CustomerService} from '../../services/customer.service';
+import {Customer} from '../../shared/models/customer';
 
 @Component({
   selector: 'app-customer-dialog',
@@ -7,28 +11,87 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
   styleUrls: ['./customer-dialog.component.scss']
 })
 export class CustomerDialogComponent implements OnInit {
+  customerId: number;
+  customerForm = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', Validators.compose([Validators.required, EmailValidator(/\w+@\w+\.\w+/)])],
+    phone: ['', Validators.required],
+    address: ['', Validators.required],
+    city: ['', Validators.required],
+    state: ['', Validators.required],
+    zipcode: ['', Validators.required],
+  });
 
   constructor(private dialogRef: MatDialogRef<CustomerDialogComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: any) {
+              @Inject(MAT_DIALOG_DATA) public data: any,
+              private fb: FormBuilder,
+              private customerService: CustomerService,
+              private snackBar: MatSnackBar) {
   }
 
   ngOnInit() {
+    if (this.data.customer) {
+      this.customerForm.patchValue(this.data.customer);
+      this.customerId = this.data.customer.id;
+    }
   }
 
   closeDialog() {
     this.dialogRef.close();
   }
 
-  saveCustomer() {
-    this.dialogRef.close({asd: 'adsasd'});
+  saveCustomer(customerParams) {
+    if (this.isNewCustomer()) {
+      this.customerService
+        .createCustomer(customerParams)
+        .subscribe(() => {
+            this.onSuccessSnackBar('Customer was created with success');
+            this.dialogRef.close({});
+          },
+          (err) => {
+            this.onFailSnackBar(err.error);
+          });
+    } else {
+      this.customerService
+        .updateCustomer(this.customerId, customerParams)
+        .subscribe(() => {
+          this.onSuccessSnackBar('Customer was updated with success');
+          this.dialogRef.close({});
+        }, (err) => {
+          this.onFailSnackBar(err.error);
+        });
+
+    }
+
+  }
+
+  onSuccessSnackBar(message) {
+    this.snackBar.open(message, null, {
+      panelClass: ['custom-snackbar', '--success']
+    });
+  }
+
+  onFailSnackBar(error) {
+    this.snackBar.open(error, null, {
+      panelClass: ['custom-snackbar', '--error']
+    });
   }
 
   actionName() {
-    return this.data.customer ? 'Edit' : 'Create';
+    return this.isNewCustomer() ? 'Create' : 'Edit';
   }
 
   actionButton() {
-    return this.data.customer ? 'Update' : 'Save';
+    return this.isNewCustomer() ? 'Save' : 'Update';
+  }
+
+  checkFieldRequired(fieldName): boolean {
+    return this.customerForm.get(fieldName).hasError('required') &&
+      this.customerForm.get(fieldName).touched;
+  }
+
+  isNewCustomer() {
+    return this.data.customer === null;
   }
 
 }
--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,15 +1,24 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SharedModule} from '../shared/shared.module';
-import { ListCustomersComponent } from './list-customers/list-customers.component';
-import { CustomerDialogComponent } from './customer-dialog/customer-dialog.component';
+import {ListCustomersComponent} from './list-customers/list-customers.component';
+import {CustomerDialogComponent} from './customer-dialog/customer-dialog.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {DestroyCustomerComponent} from './destroy-customer/destroy-customer.component';
+import {CreateCustomerComponent} from './create-customer/create-customer.component';
+import {UpdateCustomerComponent} from './update-customer/update-customer.component';
 
 
 @NgModule({
-  declarations: [ListCustomersComponent, CustomerDialogComponent],
+  declarations: [ListCustomersComponent,
+    CustomerDialogComponent,
+    DestroyCustomerComponent,
+    CreateCustomerComponent,
+    UpdateCustomerComponent],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ],
   exports: [
     ListCustomersComponent
